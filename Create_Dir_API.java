d221405f1cda1af60b383c8e983a8db072f229b6
vishal123rana <40546552+vishal123rana@users.noreply.github.com>
2020-03-03 04:51:59 -0800

    Update Create_Dir_API.java

--- Create_Dir_API.java
+++ Create_Dir_API.java
@@ -9,9 +9,11 @@ import java.net.URI;
 
 public class Create_Dir_API {
     public static void main(String []args) throws IOException{
+        //this is your hdfs path and input is folder 
         String uri = "hdfs://localhost:9000/input";
         Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(URI.create(uri),conf);
+        
+        FileSystem fs = FileSystem.get(URI.create(uri),conf); //File System object
         boolean yesorno = fs.mkdirs(new Path(uri));
         System.out.println(yesorno);
 
